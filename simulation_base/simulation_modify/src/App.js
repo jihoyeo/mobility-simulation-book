36b82a10280c6e01c1eda75ae5ecea85057a67c2
jihoyeo <jihoyeo@gachon.ac.kr>
2024-11-20 03:36:28 +0900

    update

--- simulation_base/simulation_modify/src/App.js
+++ simulation_base/simulation_modify/src/App.js
@@ -6,31 +6,44 @@ import Splash from "./components/Splash";
 import Trip from "./components/Trip";
 import "./css/app.css";
 
-/* const fetchData = (FilE_NAME) => {
-  const res = axios.get(
-    // 자신의 깃허브 주소 입력
-    // https://raw.githubusercontent.com/'자신 깃허브 이름'/simulation/main/simulation/src/data/${FilE_NAME}.json
-    `https://raw.githubusercontent.com/HNU209/simulation-class/main/simulation/src/data/${FilE_NAME}.json`
-  );
-  const data = res.then((r) => r.data);
-  return data;
-}; */
 
 const fetchData = (FilE_NAME) => {
-  return fetch(`${process.env.PUBLIC_URL}/data/${FilE_NAME}.json`)
-    .then(response => response.json());
+  const baseURL = process.env.NODE_ENV === "production"
+    ? `https://raw.githubusercontent.com/HNU209/simulation-class/main/simulation/src/data/${FilE_NAME}.json`
+    : `${process.env.PUBLIC_URL}/data/`;
+  
+  return axios.get(`${baseURL}${FilE_NAME}.json`).then((r) => r.data);
 };
 
 const App = () => {
 
-  const [trip, setTrip] = useState([]);
+  const [trip_car, setTripC] = useState([]);
+  const [trip_foot, setTripF] = useState([]);
+  const [stop, setStop] = useState([]);
+  const [point_car, setPoint] = useState([]);
 
   const [isloaded, setIsLoaded] = useState(false);
   
   const getData = useCallback(async () => {
-    const TRIP = await fetchData("trips");
 
-    setTrip((prev) => TRIP);
+    // const TRIP = await Promise.all([
+    //   fetchData("trips_car"),
+    //   fetchData("trips_foot"),
+    // ]);
+
+    const TRIP_CAR = await fetchData("trips_car");
+    const TRIP_FOOT = await fetchData("trips_foot");
+
+    const STOP = await fetchData("icon_data");
+    const POINT = await fetchData("trips_car_point");
+
+
+
+    // setTrip((prev) => TRIP.flat());
+    setTripC((prev) => TRIP_CAR);
+    setTripF((prev) => TRIP_FOOT);
+    setStop((prev) => STOP);
+    setPoint((prev) => POINT);
 
     setIsLoaded(true);
   }, []);
@@ -43,7 +56,13 @@ const App = () => {
     <div className="container">
       {!isloaded && <Splash />}
       {isloaded && (
-        <Trip trip={trip} />
+        <Trip 
+              trip_car={trip_car} 
+              trip_foot={trip_foot} 
+              stop={stop} 
+              point_car={point_car} 
+        
+        />
       )}
     </div>
   );
